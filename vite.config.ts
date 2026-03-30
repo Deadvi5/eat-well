84f7da39dd99f6fbe7cc1f7c207ebf3cba4dd2a4
Lorenzo Villa <lorenz@Mac-mini-di-Lorenzo.local>
2026-03-30 16:19:45 +0200

    chore: github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/eat-well/',
   plugins: [
     react(),
     VitePWA({ registerType: 'autoUpdate' })
